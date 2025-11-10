deec4551b7abf2d00e67d55dffe2122306acd4cb
Varunpython028 <varun_mit@techhubtechnology.com>
2025-11-11 02:28:20 +0530

    add pattern

--- 1.Day_1_Pattern/main.cpp
+++ 1.Day_1_Pattern/main.cpp
@@ -102,6 +102,27 @@ int main()
         cout<<"*"<<endl;
         cout << endl;
     }
+    /* pattern 5
+        *
+       **
+      ***
+     ****
+    */
+    for (int i = 1; i <= 4; i++)
+    {
+        
+        for (int j = 1; j <= 3; j++)
+        {
+            if(j>=4-i+1){
+                cout<<'*';
+            }
+            else{
+                cout<<' ';
+            }
+        }
+        cout<<"*"<<endl;
+        cout << endl;
+    }
 
     return 0;
 }
